b38014e6ee4c90ebc415049c7d04738119658451
rahul6975 <rahulya7569@gmail.com>
2021-08-30 09:41:12 -0700

    clickable button added

--- app/src/main/java/com/rahul/jetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/rahul/jetpackcomposedemo/MainActivity.kt
@@ -3,11 +3,19 @@ package com.rahul.jetpackcomposedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.rahul.jetpackcomposedemo.ui.theme.JetpackComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +25,8 @@ class MainActivity : ComponentActivity() {
             JetpackComposeDemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Rahul")
+//                    Greeting("Rahul")
+                    ClickableText()
                 }
             }
         }
@@ -34,5 +43,48 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     JetpackComposeDemoTheme {
         Greeting("Android")
+    }
+}
+
+
+@Composable
+fun ClickableText() {
+    var showPopup by remember { mutableStateOf(false) }
+    Column(Modifier.clickable(onClick = { showPopup = true }), content = {
+        Card(
+            shape = RoundedCornerShape(4.dp), modifier = Modifier.padding(8.dp),
+            backgroundColor = Color.LightGray
+        ) {
+            Text(
+                text = "Click to see dialog", modifier = Modifier.padding(16.dp),
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontFamily = FontFamily.Serif
+                )
+            )
+        }
+    })
+    val onPopupDismissed = { showPopup = false }
+    if (showPopup) {
+        AlertDialog(
+            onDismissRequest = onPopupDismissed,
+            text = {
+                Text("Congratulations! You just clicked the text successfully")
+            },
+            confirmButton = {
+                Button(
+                    onClick = onPopupDismissed
+                ) {
+                    Text(text = "Ok")
+                }
+            })
+    }
+}
+
+@Preview
+@Composable
+fun ClickableTextPreview() {
+    Column {
+        ClickableText()
     }
 }
