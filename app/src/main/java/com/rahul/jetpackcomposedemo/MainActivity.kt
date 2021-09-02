bfb669d17743dc50ba5ac670df50e9f97692f354
rahul6975 <rahulya7569@gmail.com>
2021-09-02 09:17:45 +0530

    rotation animations

--- app/src/main/java/com/rahul/jetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/rahul/jetpackcomposedemo/MainActivity.kt
@@ -4,18 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.TweenSpec
-import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,7 +32,8 @@ class MainActivity : ComponentActivity() {
                 Surface(color = MaterialTheme.colors.background) {
 //                    Greeting("Rahul")
 //                    ClickableText()
-                    AnimateColorComponent()
+//                    AnimateColorComponent()
+                    RotatingSquareComponent()
                 }
             }
         }
@@ -122,4 +123,36 @@ fun AnimateColorComponent() {
 @Composable
 fun AnimateColorComponentPreview() {
     AnimateColorComponent()
+}
+
+@Composable
+fun RotatingSquareComponent() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        content = {
+            val infiniteTransition = rememberInfiniteTransition()
+            val rotation by infiniteTransition.animateFloat(
+                initialValue = 0f,
+                targetValue = 360f,
+                animationSpec = infiniteRepeatable(
+                    animation = tween<Float>(
+                        durationMillis = 3000,
+                        easing = FastOutLinearInEasing,
+                    ),
+                )
+            )
+            Canvas(modifier = Modifier.size(200.dp)) {
+                rotate(rotation) {
+                    drawRect(color = Color(255, 138, 128))
+                }
+            }
+        })
+}
+
+@Preview
+@Composable
+fun RotatingSquareComponentPreview() {
+    RotatingSquareComponent()
 }
