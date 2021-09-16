fb1fc583e85edd4fc5bea94ea7ee6bf70fe1d837
rahul6975 <rahulya7569@gmail.com>
2021-09-16 09:33:46 +0530

    action button added

--- app/src/main/java/com/rahul/jetpackcomposedemo/CoroutineFlowActivity.kt
+++ app/src/main/java/com/rahul/jetpackcomposedemo/CoroutineFlowActivity.kt
@@ -25,51 +25,25 @@ import kotlinx.coroutines.flow.flow
 class CoroutineFlowActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // This is an extension function of Activity that sets the @Composable function that's
-        // passed to it as the root view of the activity. This is meant to replace the .xml file
-        // that we would typically set using the setContent(R.id.xml_file) method. The setContent
-        // block defines the activity's layout.
+        // This is an extension function of Activity that sets the @Co
         setContent {
             FlowComponent(countdownFlow())
         }
     }
 }
 
-// We represent a Composable function by annotating it with the @Composable annotation. Composable
-// functions can only be called from within the scope of other composable functions. We should
-// think of composable functions to be similar to lego blocks - each composable function is in turn
-// built up of smaller composable functions.
 @Composable
 fun FlowComponent(flow: Flow<Int>) {
-    // Here we access the coroutine flow object and convert it to a form that Jetpack Compose
-    // understands using the collectAsState method.
-
-    // Reacting to state changes is the core behavior of Compose. We use the state composable
-    // that is used for holding a state value in this composable for representing the current
-    // value of the selectedIndex. Any composable that reads the value of counter will be recomposed
-    // any time the value changes. This ensures that only the composables that depend on this
-    // will be redraw while the rest remain unchanged. This ensures efficiency and is a
-    // performance optimization. It is inspired from existing frameworks like React.
     val countDownValue by flow.collectAsState(initial = 10)
-    // Column is a composable that places its children in a vertical sequence. You
-    // can think of it similar to a LinearLayout with the vertical orientation.
-    // In addition we also pass a few modifiers to it.
-
-    // You can think of Modifiers as implementations of the decorators pattern that are
-    // used to modify the composable that its applied to. In this example, we configure the
-    // Column composable to occupy the entire available width and height using Modifier.fillMaxSize
-    // ().
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         when (countDownValue) {
-            // When the value is between 1 to 10, show the countDownValue
             in 1..10 -> {
                 CountdownText("Countdown: $countDownValue")
             }
-            // Else it's already new year, so its time to wish your friends!
             else -> {
                 CountdownText("HAPPY NEW YEAR!!!!", Color.Magenta)
             }
@@ -78,15 +52,8 @@ fun FlowComponent(flow: Flow<Int>) {
 
 }
 
-// We represent a Composable function by annotating it with the @Composable annotation. Composable
-// functions can only be called from within the scope of other composable functions. We should
-// think of composable functions to be similar to lego blocks - each composable function is in turn
-// built up of smaller composable functions.
 @Composable
 fun CountdownText(text: String, color: Color = Color.Yellow) {
-    // Text is a predefined composable that does exactly what you'd expect it to -
-    // display text on the screen. It allows you to customize its appearance using the
-    // style property.
     Text(
         text = text,
         color = color,
@@ -98,10 +65,6 @@ fun CountdownText(text: String, color: Color = Color.Yellow) {
     )
 }
 
-// This method is simply creating a coroutine flow that counts down from 9 to 0. The value is
-// updated after every 1 second. If you aren't familiar with coroutine flows, they are coroutine's
-// version of RxJava's observables/flowables. If you aren't familiar with RxJava either, think of
-// them as an open stream that notifies its observers when the value changes.
 fun countdownFlow() = flow<Int> {
     for (i in 9 downTo 0) {
         delay(1000L)
